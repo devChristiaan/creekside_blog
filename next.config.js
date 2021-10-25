d677d7d9904eee9fddf5577ab854c806b1452ed2
Christiaan Hougaard <cwhougaard@gmail.com>
2021-10-24 20:58:01 -0400

    added basic fetch post title and incorporated local settings in next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  i18n: {
+    locales: ['en-US', 'de-DL'],
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { json } from 'stream/consumers'
 import { getHeapCodeStatistics } from 'v8'
 import styles from '../styles/Home.module.scss'
+
+const api_url = process.env.API_URL
+const content_api_key = process.env.CONTENT_API_KEY
+
+type Post = {}
+
+async function getPosts() {
+
+  const res = await fetch(`${api_url}/ghost/api/v3/content/posts/?key=${content_api_key}`)
+  const payload = await res.json()
+
+  let titles = payload.posts.map(post => post.title)
+
+  console.log(titles);
+  
+
+  return titles
+}
 
 export const getStaticProps = async ({ params }) => {
   const posts = await getPosts()
@@ -11,10 +30,12 @@ export const getStaticProps = async ({ params }) => {
   }
 }
 
-export default function Home() {
+const Home: React.FC<{ posts: Post[] }> = (props) => {
   return (
     <div className={styles.container}>
-      
+      <h1>`Hello :)`</h1>
     </div>
   )
 }
+
+export default Home
