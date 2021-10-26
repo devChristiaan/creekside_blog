2875cc95d787b1b18da81c6b80151511e655f52f
Christiaan Hougaard <cwhougaard@gmail.com>
2021-10-25 20:24:47 -0400

    created simple blog home page fetching content from ghost

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,19 @@ import styles from '../styles/Home.module.scss'
 const api_url = process.env.API_URL
 const content_api_key = process.env.CONTENT_API_KEY
 
-type Post = {}
+type Post = {
+  title: string
+  slug: string
+  feature_image: string
+  custome_excerpt: string
+}
 
 async function getPosts() {
 
-  const res = await fetch(`${api_url}/ghost/api/v3/content/posts/?key=${content_api_key}`)
+  const res = await fetch(`${api_url}/ghost/api/v3/content/posts/?key=${content_api_key}&fields=title,slug,custome_excerpt,feature_image`)
   const payload = await res.json()
 
-  let titles = payload.posts.map(post => post.title)
-
-  console.log(titles);
-  
-
-  return titles
+  return payload.posts
 }
 
 export const getStaticProps = async ({ params }) => {
@@ -30,11 +30,18 @@ export const getStaticProps = async ({ params }) => {
 
 const Home: React.FC<{ posts: Post[] }> = (props) => {
 
+  const { posts } = props
+
   const router = useRouter()
 
   return (
     <div className={styles.container}>
-      <h1>`Hello :)`</h1>
+      <h1>Hello World</h1>
+      <ul>
+        {posts.map((post, index) => {
+          return <li key={post.slug}><Link href="/post/[slug]" as={`/post/${post.slug}`}><a>{post.title}</a></Link></li>
+        })}
+      </ul>
       <ul>
         {router.locales.map(lan => (
          <li key={lan}>
