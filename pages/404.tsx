2b7d8a07fb10713f91e49548a6573b05de3c746b
Christiaan Hougaard <cwhougaard@gmail.com>
2021-10-27 20:46:06 -0400

    added custome 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Custome404: React.FC = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  )
+}
+
+export default Custome404
